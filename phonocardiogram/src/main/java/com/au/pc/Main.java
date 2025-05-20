8258404540f5623f09a7254b9cb0f5e68d09aa1f
CAPELLAX02 <ahmtatar@gmail.com>
2025-05-20 20:19:12 +0300

    Initialize the interface using JavaFX.

--- phonocardiogram/src/main/java/com/au/pc/Main.java
+++ phonocardiogram/src/main/java/com/au/pc/Main.java
@@ -1,7 +1,19 @@
 package com.au.pc;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setScene(new Scene(new Label("JavaFX working..."), 300, 100));
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("coming soon...");
+        launch(args);
     }
 }
---
+++ phonocardiogram/src/main/java/com/au/pc/visual/WaveformCanvas.java
@@ -0,0 +1,4 @@
+package com.au.pc.visual;
+
+public class WaveformCanvas {
+}
